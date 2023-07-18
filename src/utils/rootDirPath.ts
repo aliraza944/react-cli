1a5170817633c53c83b6bbc079525fe62f9db5cd
aliraza944 <alisabri022@gmail.com>
2023-07-18 23:56:41 +0500

    this will find the project root directory by checking the lavel at which our pakage.json file is present

--- src/utils/rootDirPath.ts
+++ src/utils/rootDirPath.ts
@@ -1,5 +1,26 @@
-import * as Path from "path";
+import * as path from "path";
 import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
+import * as fs from "fs";
 
-export const __dirname = Path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+let currentDir = path.dirname(__filename);
+
+while (true) {
+  const packageJsonPath = path.join(currentDir, "package.json");
+
+  if (fs.existsSync(packageJsonPath)) {
+    break; // Found the package.json file
+  }
+
+  const parentDir = path.dirname(currentDir);
+
+  // Check if we reached the root directory
+  if (currentDir === parentDir) {
+    throw new Error("package.json not found in the directory tree.");
+  }
+
+  currentDir = parentDir;
+}
+
+const __dirname = currentDir;
+export { __dirname };
