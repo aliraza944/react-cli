8d3b6dadade4acded71b54a9f6ff1003fcccb828
aliraza944 <alisabri022@gmail.com>
2023-07-22 23:14:04 +0500

    Added the options for the custom hoook

--- src/actions/createComponent.ts
+++ src/actions/createComponent.ts
@@ -1,48 +1,42 @@
 import * as Path from "path";
-import * as fs from "fs";
 import { upperCaseName } from "../utils/upperCaseName.js";
 import { ComponentTemplate, IndexTemplate } from "../templates/component.js";
 import { isTypeScriptProject } from "../utils/typeScriptSelection.js";
 import { createFile } from "../utils/createFile.js";
-export const createComponent = (name: string) => {
+import { customHook } from "../templates/customHook.js";
+import { lookForFolder } from "../utils/lookForFolder.js";
+export const createComponent = (name: string, hook = false) => {
   const componentName = upperCaseName(name);
-  createComponentsFile(componentName);
+  createComponentsFile(componentName, hook);
 };
 
-const lookForComponentsFolder = (componentsDir: string) => {
-  const isComponentDirExits = fs.existsSync(componentsDir);
-
-  // if not create a new directory
-  if (!isComponentDirExits) {
-    try {
-      fs.mkdirSync(componentsDir, { recursive: true });
-    } catch (error) {
-      console.error("Error creating the directory:", error);
-    }
-    return;
-  }
-  console.error("The component already exists");
-  process.exit(1);
-};
-
-const createComponentsFile = (name: string) => {
+const createComponentsFile = (name: string, hook = false) => {
   const fileContent = ComponentTemplate(name);
   const indexFileContent = IndexTemplate(name);
+  const customHookFileContent = customHook(name);
   const componentsDir = Path.resolve(
     process.cwd(),
     `src`,
     "components",
     `${name}`
   );
-
-  const fileExtension = isTypeScriptProject ? ".tsx" : ".jsx";
+  const customHooksDir = Path.resolve(process.cwd(), `src`, `${hook}`);
+  const fileExtension = isTypeScriptProject ? ".ts" : ".js";
 
   // check if the components directory exists
-  lookForComponentsFolder(componentsDir);
-  const filePath = Path.join(componentsDir, name + `${fileExtension}`);
-  const indexFilePath = Path.join(componentsDir, "index" + `${fileExtension}`);
-
-  const successMessage = `Component ${name}${fileExtension} created successfully`;
+  lookForFolder(componentsDir, true);
+  // check if the custom hooks directory exists
+  if (typeof hook === "string") {
+    lookForFolder(customHooksDir);
+  }
+  const filePath = Path.join(componentsDir, name + `${fileExtension}x`);
+  const indexFilePath = Path.join(componentsDir, "index" + `${fileExtension}x`);
+  const customHookFilePath = Path.join(
+    typeof hook === "string" ? customHooksDir : componentsDir,
+    `use${name}` + `${fileExtension}`
+  );
+  const successMessage = `Component ${name}${fileExtension}x created successfully`;
   createFile(filePath, fileContent);
+  createFile(customHookFilePath, customHookFileContent);
   createFile(indexFilePath, indexFileContent, successMessage);
 };
--- src/utils/cli.ts
+++ src/utils/cli.ts
@@ -1,10 +1,16 @@
 import { Command } from "commander";
 import { createComponent } from "../actions/createComponent.js";
 const program = new Command();
-
+//TODO: add version
 program.name("react cli").description("CLI to quickly create react components");
 
-program.command("component <name>").action((name) => {
-  createComponent(name);
-});
+program
+  .command("component <name>")
+  .option(
+    "-h, --hook [specified-folder-name]",
+    "create the components custom hook"
+  )
+  .action((name, option) => {
+    createComponent(name, option.hook);
+  });
 export default program;
